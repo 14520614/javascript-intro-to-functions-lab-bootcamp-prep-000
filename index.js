1296ecbb7a82f6cbcd2f550815cc9580c69037dc
Trịnh Bảo Khai Nguyên <14520614@gm.uit.edu.vn>
2017-11-25 02:32:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,3 +14,15 @@ function logWhisper(string)
 {
   return console.log(string.toLowerCase())
 }
+function sayHiToGrandma(string)
+{
+if ( string === string.toLowerCase())
+{
+  return console.log("I can't hear you")
+}
+else if (string === string.toUpperCase()) 
+{
+  return console.log("YES INDEED!")
+}
+else return console.log("I love you, too") 
+}
